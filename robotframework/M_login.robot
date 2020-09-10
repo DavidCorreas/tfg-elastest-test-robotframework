4ae128d0939ba16d3aeddc66f8331c482cac6dd8
DavidCorreas <davidestu77@gmail.com>
2020-09-10 12:02:17 +0200

    Added: Test Setup

--- robotframework/M_login.robot
+++ robotframework/M_login.robot
@@ -6,23 +6,42 @@ Library    po/common/PythonPathScript.py
 
 Library    po-mobil/MobApplication.py
 Library    po-mobil/MobLogin.py
+Library    po/common/Common.py
 
 *** Variables ***
 
 
 *** Test Cases ***
-
-LOGIN-0001
+TEST-SETUP
+    [Documentation]  Test necesario para crear el usuario si no existe
     Comment  Abrir Aplicacion
     MobApplication.Abrir aplicacion movil en Android
 
-    Comment    Login basico como administrador
+    Comment    Intentamos registrar a David si no existe su usuario
     MobApplication.Capturar Pantallazo movil
-    MobLogin.Registrarse como David
-    MobLogin.Logarse como David
+    MobLogin.Registrar a David si no existe
 
-#    MobApplication.Capturar Pantallazo movil
-#    MobLogin.Deslogarse
-#    MobApplication.Capturar Pantallazo movil
+    Comment  Cerramos la aplicacion
+    MobApplication.Capturar Pantallazo movil
+    MobApplication.Cerrar Aplicacion movil
 
+LOGIN-0001
+    [Documentation]  Registro y loging con un usuario aleatorio
+    ${USER}  Common.Randomize  user@random  4
+    ${PASSWORD}  Common.Randomize  passwd
+
+    Comment  Abrir Aplicacion
+    MobApplication.Abrir aplicacion movil en Android
+
+    Comment    SingUp
+    MobApplication.Capturar Pantallazo movil
+    MobLogin.Registrarse con email ${USER} y contrasena ${PASSWORD}
+
+    Comment  LogIn
+    MobLogin.Logarse con email ${USER} y contrasena ${PASSWORD}
+
+    Comment  Logout y cerramos la aplicacion
+    MobApplication.Capturar Pantallazo movil
+    MobLogin.Deslogarse
+    MobApplication.Capturar Pantallazo movil
     MobApplication.Cerrar Aplicacion movil
--- robotframework/po-mobil/MobLogin.py
+++ robotframework/po-mobil/MobLogin.py
@@ -3,6 +3,7 @@ import json
 import os
 
 from robot.api.deco import keyword
+from robot.libraries.BuiltIn import BuiltIn
 from robotframework.po.common.PageObject import PageObject
 
 ROBOT_LIBRARY_DOC_FORMAT = 'HTML'
@@ -16,6 +17,11 @@ btn_submit_login = "//button[@type='submit']"
 # SingUp
 btn_top_singup = "//a[@routerlink='/auth/singup']"
 btn_submit_singup = "//button[@type='submit']"
+dialog_error = "//mat-dialog-container"
+btn_accept_error = dialog_error + "//button"
+
+# Logout
+btn_logout = "//button/*[text()='LogOut']"
 
 # Pagina posts
 list_posts = "//app-post-list"
@@ -35,34 +41,76 @@ class MobLogin(PageObject):
             self.user_data = json.load(user_file)
 
     # ------------------------------- Keywords ------------------------------- #
-    @keyword(name='Registrarse como ${rol}')
-    def sing_up(self, rol):
+    @keyword(name='Registrarse con email ${email} y contrasena ${password}')
+    def singup_with_credentials(self, email, password):
         # Accedemos a la pagina para registrarnos
         self.osl.wait_until_element_is_visible(btn_top_singup)
         self.osl.click_element(btn_top_singup)
         self.osl.wait_until_element_is_visible(cmp_email)
         
         # Introducimos credenciales
-        self.osl.input_text(cmp_email, self.user_data["Credenciales"][rol]["Email"])
-        self.osl.input_text(cpm_password, self.user_data["Credenciales"][rol]["Password"])
+        self.osl.input_text(cmp_email, email)
+        self.osl.input_text(cpm_password, password)
 
         # Clickamos para registrarnos
         self.osl.click_element(btn_submit_singup)
         self.osl.wait_until_element_is_visible(list_posts)
-    
-    @keyword(name='Logarse como ${rol}')
-    def login_as(self, rol):
+
+    @keyword(name='Registrarse como ${rol}')
+    def sing_up(self, rol):
+        email = self.user_data["Credenciales"][rol]["Email"]
+        password = self.user_data["Credenciales"][rol]["Password"]
+        self.singup_with_credentials(email, password)
+
+    @keyword(name='Registrar a ${rol} si no existe')
+    def sing_up_if_not_exists(self, rol):
+        BuiltIn().run_keyword_and_ignore_error('Registrarse como ' + rol)
+        try:
+            self._accept_error()
+        except Exception as e:
+            print(e)
+
+    @keyword(name='Intentar volver a registrarse como ${rol}')
+    def try_login(self, rol):
+        # Intentamos logarnos, pero falla
+        BuiltIn().run_keyword_and_ignore_error('Registrarse como ' + rol)
+        try:
+            # TODO: BORRAR LOGS
+            BuiltIn().log_to_console('Intentamos cerrar el error')
+            self._accept_error()
+            BuiltIn().log_to_console('Cerrado conrrectamente')
+        except Exception as e:
+            print(e)
+
+    @keyword(name='Logarse con email ${email} y contrasena ${password}')
+    def login_with_credentials(self, email, password):
         # Accedemos al login.
         self.osl.wait_until_element_is_visible(btn_top_login)
         self.osl.click_element(btn_top_login)
         self.osl.wait_until_element_is_visible(cmp_email)
         
         # Introducimos credenciales
-        self.osl.input_text(cmp_email, self.user_data["Credenciales"][rol]["Email"])
-        self.osl.input_text(cpm_password, self.user_data["Credenciales"][rol]["Password"])
+        self.osl.input_text(cmp_email, email)
+        self.osl.input_text(cpm_password, password)
 
         # Clickamos logearnos
         self.osl.click_element(btn_submit_login)
         self.osl.wait_until_element_is_visible(list_posts)
 
-        
+    @keyword(name='Logarse como ${rol}')
+    def login_as(self, rol):
+        email = self.user_data["Credenciales"][rol]["Email"]
+        password = self.user_data["Credenciales"][rol]["Password"]
+        self.login_with_credentials(email, password)
+
+    @keyword(name='Deslogarse')
+    def logout(self):
+        # Clicamos el logout
+        self.osl.wait_until_element_is_visible(btn_logout)
+        self.osl.click_element(btn_logout)
+        self.osl.wait_until_element_is_visible(btn_top_login)
+
+    def _accept_error(self):
+        # Aceptamos el error
+        self.osl.wait_until_element_is_visible(dialog_error)
+        self.osl.click_element(btn_accept_error)
