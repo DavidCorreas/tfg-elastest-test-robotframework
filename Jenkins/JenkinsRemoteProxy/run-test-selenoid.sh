d63d7ec05eec68b1e2e810763821f70d358353b1
DavidCorreas <davidestu77@gmail.com>
2020-12-02 01:46:42 +0100

    Fixed: selenoid net

--- Jenkins/JenkinsRemoteProxy/run-test-selenoid.sh
+++ Jenkins/JenkinsRemoteProxy/run-test-selenoid.sh
@@ -3,8 +3,8 @@
 # JOB_NAME is the name of the project of this build. This is the name you gave your job. It is set up by Jenkins
 COMPOSE_ID=${JOB_NAME:-local}
 # Remove Previous Stack
-docker-compose -f Jenkins/JenkinsRemoteProxy/docker-compose.yml -f selenoid_network-compose.yml -p $COMPOSE_ID rm -f
+docker-compose -f Jenkins/JenkinsRemoteProxy/docker-compose.yml -f Jenkins/JenkinsRemoteProxy/selenoid_network-compose.yml -p $COMPOSE_ID rm -f
 # Starting new stack environment
-docker-compose -f Jenkins/JenkinsRemoteProxy/docker-compose.yml -f selenoid_network-compose.yml -p $COMPOSE_ID up --build
+docker-compose -f Jenkins/JenkinsRemoteProxy/docker-compose.yml -f Jenkins/JenkinsRemoteProxy/selenoid_network-compose.yml -p $COMPOSE_ID up --build
 
-docker-compose -f Jenkins/JenkinsRemoteProxy/docker-compose.yml -f selenoid_network-compose.yml -p $COMPOSE_ID rm -f
+docker-compose -f Jenkins/JenkinsRemoteProxy/docker-compose.yml -f Jenkins/JenkinsRemoteProxy/selenoid_network-compose.yml -p $COMPOSE_ID rm -f
