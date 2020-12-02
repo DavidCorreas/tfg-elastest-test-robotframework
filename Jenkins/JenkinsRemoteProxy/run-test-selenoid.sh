1498db3bd2ac03c98a84a6cc932c73bf9d5188ac
DavidCorreas <davidestu77@gmail.com>
2020-12-02 01:44:43 +0100

    Added: selenoid network

---
+++ Jenkins/JenkinsRemoteProxy/run-test-selenoid.sh
@@ -0,0 +1,10 @@
+# Usar si el test tiene acceso a la red del selenoid.
+
+# JOB_NAME is the name of the project of this build. This is the name you gave your job. It is set up by Jenkins
+COMPOSE_ID=${JOB_NAME:-local}
+# Remove Previous Stack
+docker-compose -f Jenkins/JenkinsRemoteProxy/docker-compose.yml -f selenoid_network-compose.yml -p $COMPOSE_ID rm -f
+# Starting new stack environment
+docker-compose -f Jenkins/JenkinsRemoteProxy/docker-compose.yml -f selenoid_network-compose.yml -p $COMPOSE_ID up --build
+
+docker-compose -f Jenkins/JenkinsRemoteProxy/docker-compose.yml -f selenoid_network-compose.yml -p $COMPOSE_ID rm -f
